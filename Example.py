452f5fab7486b752ad4b9c5b12d6904ffe079c93
nirmalk-git <nirmalk90@gmail.com>
2022-07-07 12:13:08 +0200

    Actual code created

---
+++ Example.py
@@ -0,0 +1,29 @@
+
+
+def Multiplication(arg1, arg2):
+    """
+    Functions take two argurements and multiply them.
+    :type arg2: object
+    :param arg1:
+    :type arg1:
+    :param arg2:
+    :type arg2:
+    :return arg1*arg2:
+    :rtype:
+    """
+    mul = arg1*arg2
+    return mul
+
+
+def division(arg1, arg2):
+    """
+    Function to calculate the divided value. 
+    :param arg1:
+    :type arg1:
+    :param arg2:
+    :type arg2:
+    :return:
+    :rtype:
+    """
+    div = arg1/arg2
+    return div
